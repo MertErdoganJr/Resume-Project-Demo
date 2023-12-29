85964df6f168966ca5c6f0ac13a180cdf138e727
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-12-29 20:26:20 +0300

    Final

---
+++ ResmeProject/Controllers/WebProjectsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ResmeProject.Models;
+
+namespace ResmeProject.Controllers
+{
+    public class WebProjectsController : Controller
+    {
+        DbResumeEntities db = new DbResumeEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialServices()
+        {
+            var values = db.TblProject.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialScripts()
+        {
+            return PartialView();
+        }
+    }
+}
